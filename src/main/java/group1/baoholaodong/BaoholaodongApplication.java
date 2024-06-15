da437b450a9facaaa83cc88c475c8fc83a8895c6
Nguyen Thanh Tung <ngthtunghhtb@gmail.com>
2024-06-15 09:03:15 +0700

    first commit

---
+++ src/main/java/group1/baoholaodong/BaoholaodongApplication.java
@@ -0,0 +1,13 @@
+package group1.baoholaodong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaoholaodongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaoholaodongApplication.class, args);
+	}
+
+}
