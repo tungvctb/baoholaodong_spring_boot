abd4efdbf9158d8fb9dbffa3ed4dc82c20e21b3d
vutruong0412@gmail.com <vutruong0412@gmail.com>
2024-07-02 20:50:15 +0700

    A Truong chinh them layout

--- src/main/java/group1/baoholaodong/controller/HomeController.java
+++ src/main/java/group1/baoholaodong/controller/HomeController.java
@@ -2,8 +2,12 @@ package group1.baoholaodong.controller;
 
 import group1.baoholaodong.dao.CategoryDAO;
 import group1.baoholaodong.models.Category;
+import group1.baoholaodong.services.MajorService;
+import group1.baoholaodong.models.Major;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,16 +18,20 @@ import java.util.List;
 public class HomeController {
     @Autowired
     private CategoryDAO categoryDAO;
+    @Autowired
+    private MajorService majorService;
 
     @GetMapping("")
     public String index(){
         List<Category> categories = categoryDAO.findAll();
         return "category/index";
     }
-        
+ // a Truong them phan Index cho Customer
     @GetMapping("/demo")
-    public String demo() {
+    public String demo(Model model) {
+    	List<Major> ls = majorService.findAll();
+    	model.addAttribute("ls",ls);
     	return "/customer/index";
     }
-    
 }
+    
